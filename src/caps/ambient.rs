79000949147e321b12e1cc57fb9c7170e8031b71
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-11-03 14:51:46 -0500

    test(caps): add more complete ambient capability tests
    Of course, there's a limit to how well these can be tested.

--- src/caps/ambient.rs
+++ src/caps/ambient.rs
@@ -108,8 +108,53 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_ambient() {
-        probe().unwrap();
-        assert!(is_supported());
+    fn test_ambient_supported() {
+        if is_supported() {
+            let orig_caps = probe().unwrap();
+            let supported_caps = Cap::probe_supported();
+
+            // Make sure everything is consistent
+            for cap in Cap::iter() {
+                if orig_caps.has(cap) {
+                    assert_eq!(is_set(cap), Some(true));
+                } else if supported_caps.has(cap) {
+                    assert_eq!(is_set(cap), Some(false));
+                } else {
+                    assert_eq!(is_set(cap), None);
+                }
+            }
+
+            // Clear the ambient capability set
+            clear().unwrap();
+
+            // Now make sure it's actually empty
+            assert_eq!(probe().unwrap(), CapSet::empty());
+
+            // Raise all the capabilities that were in there originally
+            for cap in orig_caps.iter() {
+                raise(cap).unwrap();
+            }
+
+            // Lower all the ones that weren't
+            for cap in (supported_caps - orig_caps).iter() {
+                lower(cap).unwrap();
+            }
+
+            for cap in !supported_caps {
+                assert_eq!(raise(cap).unwrap_err().raw_os_error(), Some(libc::EINVAL));
+                assert_eq!(lower(cap).unwrap_err().raw_os_error(), Some(libc::EINVAL));
+            }
+        } else {
+            assert_eq!(probe(), None);
+            assert_eq!(
+                raise(Cap::CHOWN).unwrap_err().raw_os_error(),
+                Some(libc::EINVAL)
+            );
+            assert_eq!(
+                lower(Cap::CHOWN).unwrap_err().raw_os_error(),
+                Some(libc::EINVAL)
+            );
+            assert_eq!(clear().unwrap_err().raw_os_error(), Some(libc::EINVAL));
+        }
     }
 }
